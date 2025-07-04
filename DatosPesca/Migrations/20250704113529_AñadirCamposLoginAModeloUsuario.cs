d3d70f407ee1461c9bb81be0668c54f0b775f2f0
amse <amse794@gmail.com>
2025-07-04 13:37:05 +0200

    Actualizado el nuevo modelo usuario en la base de datos

---
+++ DatosPesca/Migrations/20250704113529_AñadirCamposLoginAModeloUsuario.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatosPesca.Migrations
+{
+    /// <inheritdoc />
+    public partial class AñadirCamposLoginAModeloUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Contraseña",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Correo",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contraseña",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "Correo",
+                table: "Usuarios");
+        }
+    }
+}
